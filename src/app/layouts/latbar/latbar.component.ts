e88b16ce914148d9c436cacb90149e688d7f726a
Daniel Garcia Uscanga <dalokdaga@gmail.com>
2021-05-02 14:52:35 -0500

    configuracion para gestion de usuarios acutalizado desde casa

--- src/app/layouts/latbar/latbar.component.ts
+++ src/app/layouts/latbar/latbar.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./latbar.component.css']
 })
 export class LatbarComponent implements OnInit {
-
+  muestraUsuarios:boolean;
+  muestaModulos:boolean;
+  nombre:string;
   constructor() { }
 
   ngOnInit() {
+    this.muestraUsuarios = false;
+    this.muestaModulos = false;
+    this.nombre = "Daniel Garcia"
   }
 
+  oculta(){
+    this.muestraUsuarios = !this.muestraUsuarios;
+    this.muestaModulos = false;
+  }
+  ocultaModulo(){
+    this.muestaModulos = !this.muestaModulos;
+    this.muestraUsuarios = false;
+  }
 }
